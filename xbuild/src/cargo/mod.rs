142e69e16a07b18ffbea6ee1ce31855c4fb98c3b
Martin Marmsoler <martin.marmsoler@gmail.com>
2025-11-07 18:26:27 +0100

    Show path name in error message

--- xbuild/src/cargo/mod.rs
+++ xbuild/src/cargo/mod.rs
@@ -38,7 +38,7 @@ impl Cargo {
             .map(|path| {
                 if path.file_name() != Some(OsStr::new("Cargo.toml")) || !path.is_file() {
                     Err(anyhow::anyhow!(
-                        "The manifest-path must be a path to a Cargo.toml file"
+                        "The manifest-path '{:?}' must be a path to a Cargo.toml file", path
                     ))
                 } else {
                     Ok(path)
